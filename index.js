a0efd5f287b25dc3aa8f2ec64683ac49bd359843
pockks <petervolosenco@gmail.com>
2022-03-16 16:34:16 +0000

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // run `node index.js` in the terminal
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hi.');
+  res.render('main', {});
 });
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname);
+app.use(express.static(path.join(__dirname, '')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(8080);
