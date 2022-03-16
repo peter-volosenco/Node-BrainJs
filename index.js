1c5e0e3f4cacc5a001cf8b76006771170dcecc14
pockks <petervolosenco@gmail.com>
2022-03-16 16:19:28 +0000

    3 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-// provide optional config object (or undefined). Defaults shown.
-var brain = require('brain.js');
+// run `node index.js` in the terminal
+var express = require('express');
 
-const config = {
-  binaryThresh: 0.5,
-  hiddenLayers: [3], // array of ints for the sizes of the hidden layers in the network
-  activation: 'sigmoid', // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh'],
-  leakyReluAlpha: 0.01, // supported for activation type 'leaky-relu'
-};
+var app = express();
 
-// create a simple feed forward neural network with backpropagation
-const net = new brain.NeuralNetwork(config);
+app.get('/', function (req, res) {
+  res.send('Hi.');
+});
 
-net.train([
-  { input: [0, 0], output: [0] },
-  { input: [0, 1], output: [1] },
-  { input: [1, 0], output: [1] },
-  { input: [1, 1], output: [0] },
-]);
-
-const output = net.run([1, 0]); // [0.987]
+app.listen(8080);
